6522fe73080da283e2d864da9d9e72d3dc2d9178
Durjoy-dcp <unknownccxv@gmail.com>
2023-02-08 19:21:43 +0600

    api setup done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"> Hello world</div>;
+  const [quote, setQuote] = useState({});
+  const showQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data);
+        console.log(data);
+        // console.log(newaray);
+      });
+  };
+
+  return (
+    <div className="App">
+      <h5>{quote.content} </h5>
+      <button onClick={showQuote}>Fetch</button>
+    </div>
+  );
 }
 
 export default App;
